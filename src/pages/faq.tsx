33cf80701959e6a8535f9f8c8e6889a0eea07d48
Monica Henao <monicahenao2211@gmail.com>
2023-12-14 18:54:29 -0500

    Update details services and page faq

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,7 +1,144 @@
 export const Faq = () => {
-    return (
-      <>
-        <a href={`/faq`} />
-      </>
-    );
-  };
+  return (
+    <>
+      <a href={`/faq`} />
+      {/* <!-- Header Banner --> */}
+      <div
+        className="banner-header valign bg-img bg-fixed"
+        data-overlay-dark="3"
+        data-background="img/slider/7.jpg"
+      >
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 text-end caption">
+              <h1>
+                Popular <span>Questions</span>
+              </h1>
+              <h5>Frequently Asked Questions</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/*<!-- Faqs -->  */}
+      <section className="section-padding">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <ul className="accordion-box clearfix">
+                <li className="accordion block">
+                  <div className="acc-btn size-20">
+                    Where Are The Fabrics Sourced?
+                  </div>
+                  <div className="acc-content">
+                    <div className="content">
+                      <div className="text">
+                        Lorem ut nisl quam nestibulum ac quam nec odio elementum
+                        sceisue the aucan ligula. Orci varius natoque penatibus
+                        et magnis dis parturient monte nascete ridiculus mus
+                        nellentesque habitant morbine.
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li className="accordion block">
+                  <div className="acc-btn size-20">
+                    How Long Does It Take To Make A Garment?
+                  </div>
+                  <div className="acc-content">
+                    <div className="content">
+                      <div className="text">
+                        Lorem ut nisl quam nestibulum ac quam nec odio elementum
+                        sceisue the aucan ligula. Orci varius natoque penatibus
+                        et magnis dis parturient monte nascete ridiculus mus
+                        nellentesque habitant morbine.
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li className="accordion block">
+                  <div className="acc-btn size-20">
+                    Do You Make Ladies wear?
+                  </div>
+                  <div className="acc-content">
+                    <div className="content">
+                      <div className="text">
+                        Lorem ut nisl quam nestibulum ac quam nec odio elementum
+                        sceisue the aucan ligula. Orci varius natoque penatibus
+                        et magnis dis parturient monte nascete ridiculus mus
+                        nellentesque habitant morbine.
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li className="accordion block">
+                  <div className="acc-btn size-20">
+                    How long will it take to complete my order?
+                  </div>
+                  <div className="acc-content">
+                    <div className="content">
+                      <div className="text">
+                        Lorem ut nisl quam nestibulum ac quam nec odio elementum
+                        sceisue the aucan ligula. Orci varius natoque penatibus
+                        et magnis dis parturient monte nascete ridiculus mus
+                        nellentesque habitant morbine.
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-6">
+              <ul className="accordion-box clearfix">
+                <li className="accordion block">
+                  <div className="acc-btn size-20">
+                    Which countries do you currently ship to?
+                  </div>
+                  <div className="acc-content">
+                    <div className="content">
+                      <div className="text">
+                        Lorem ut nisl quam nestibulum ac quam nec odio elementum
+                        sceisue the aucan ligula. Orci varius natoque penatibus
+                        et magnis dis parturient monte nascete ridiculus mus
+                        nellentesque habitant morbine.
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li className="accordion block">
+                  <div className="acc-btn size-20">
+                    Where are your products manufactured?
+                  </div>
+                  <div className="acc-content">
+                    <div className="content">
+                      <div className="text">
+                        Lorem ut nisl quam nestibulum ac quam nec odio elementum
+                        sceisue the aucan ligula. Orci varius natoque penatibus
+                        et magnis dis parturient monte nascete ridiculus mus
+                        nellentesque habitant morbine.
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li className="accordion block">
+                  <div className="acc-btn size-20">
+                    What alterations do you offer?
+                  </div>
+                  <div className="acc-content">
+                    <div className="content">
+                      <div className="text">
+                        Lorem ut nisl quam nestibulum ac quam nec odio elementum
+                        sceisue the aucan ligula. Orci varius natoque penatibus
+                        et magnis dis parturient monte nascete ridiculus mus
+                        nellentesque habitant morbine.
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
--- src/pages/myServices.tsx
+++ src/pages/myServices.tsx
@@ -1,11 +1,10 @@
-
 import { Image3, Image4, Image5, Image6, Image7, Image9 } from "../assets";
 import { AppointmentForm } from "../components/organisms";
 
 export const MyServices = () => {
   return (
     <>
-      <a href={`/myservices`} />
+      <a href={`/services`} />
       {/* <!-- Header Banner --> */}
       <div
         className="banner-header valign bg-img bg-fixed"
@@ -30,11 +29,7 @@ export const MyServices = () => {
             >
               <div className="item right">
                 <figure>
-                  <img
-                    src={Image3}
-                    alt=""
-                    className="img-fluid"
-                  />
+                  <img src={Image3} alt="" className="img-fluid" />
                 </figure>
                 <div className="caption padding-left">
                   <div className="title">Personal Shopping</div>
@@ -42,12 +37,14 @@ export const MyServices = () => {
                     En nuestra rutina diaria, el tiempo es un recurso valioso.
                     Yo tomo la responsabilidad de investigar las tendencias,
                     explorar tiendas y seleccionar opciones que se ajusten a tus
-                    preferencias. Esto no solo ahorra tiempo, sino también tu
-                    energía. Cada persona es única, al igual que su estilo. Un
-                    personal shopper trabaja contigo para comprender tus
-                    preferencias, tu cuerpo y tu personalidad, y selecciona
-                    prendas que resalten lo mejor de ti. La ropa no solo se
-                    trata de seguir tendencias, sino de expresar quién eres.
+                    preferencias y necesidades. Esto no solo ahorra tiempo, sino
+                    también tu energía. Cada persona es única, al igual que su
+                    estilo. Voy a trabajar contigo para comprender tu cuerpo y
+                    tu personalidad, y así seleccionar prendas que resalten lo
+                    mejor de ti. La ropa no solo se trata de seguir tendencias,
+                    sino de expresar quién eres. La confidencialidad es uno de
+                    nuestros pilares, mantener la información segura tuya es de
+                    vital importancia.
                   </p>
                   <a href={`/contact`} className="button-1">
                     Contáctame<span></span>
@@ -61,19 +58,24 @@ export const MyServices = () => {
             >
               <div className="item">
                 <figure>
-                  <img
-                    src={Image4}
-                    alt=""
-                    className="img-fluid"
-                  />
+                  <img src={Image4} alt="" className="img-fluid" />
                 </figure>
                 <div className="caption padding-left">
                   <div className="title">Revisión de tu closet</div>
                   <p>
-                    Yo examinaré tu closet, revisando prendas, accesorios y
+                    Examiremos tu closet, revisando prendas, accesorios y
                     calzado. Se identificarán las piezas clave, los elementos
                     favoritos y aquellas prendas que ya no cumplen con las
-                    expectativas del cliente.
+                    expectativas tuyas. Analizaré tu estilo actual y buscaremos
+                    comprender cómo las prendas en el closet reflejan tu
+                    personalidad y estilo de vida. En colaboración conjunta,
+                    decidiremos qué prendas mantener, cuáles donar o descartar y
+                    cuáles podrían necesitar reparaciones o modificaciones.
+                    Organizaremos el closet de manera eficiente para facilitar
+                    la visualización y el acceso a las prendas. Crearemos
+                    conjuntos con las prendas existentes para mostrarte nuevas
+                    formas de usar lo que ya posees. Y porque no? comprar
+                    prendas basicas que te esten faltando en tu closet
                   </p>
                   <a href={`/contact`} className="button-1">
                     Contáctame<span></span>
@@ -87,11 +89,7 @@ export const MyServices = () => {
             >
               <div className="item right">
                 <figure>
-                  <img
-                    src={Image5}
-                    alt=""
-                    className="img-fluid"
-                  />
+                  <img src={Image5} alt="" className="img-fluid" />
                 </figure>
                 <div className="caption padding-left">
                   <div className="title">Qué llevar en tu viaje</div>
@@ -99,7 +97,12 @@ export const MyServices = () => {
                     Con este servicio para la planificación de un viaje, vamos a
                     empacar de manera eficiente y asegurarnos de que lleves
                     consigo prendas versátiles y apropiadas para todas las
-                    ocasiones previstas.
+                    ocasiones previstas. Comprenderemos el proposito del viaje,
+                    duración, eventos, clima del destino, para asegurarnos que
+                    no se te pase nada. Verificaremos tu closet para realizar
+                    conjuntos versátiles y así combinar atuendos, tecnicas de
+                    empacado eficiente y en caso de que sea necesario comprar
+                    algo adicional para estar preparados para el viaje.
                   </p>
                   <a href={`/contact`} className="button-1">
                     Contáctame<span></span>
@@ -113,11 +116,7 @@ export const MyServices = () => {
             >
               <div className="item">
                 <figure>
-                  <img
-                    src={Image6}
-                    alt=""
-                    className="img-fluid"
-                  />
+                  <img src={Image6} alt="" className="img-fluid" />
                 </figure>
                 <div className="caption padding-left">
                   <div className="title">Asesoría para hombres</div>
@@ -126,7 +125,14 @@ export const MyServices = () => {
                     importante tambien es el del hombre por eso en esta asesoría
                     brindaré orientación y asistencia en la selección de prendas
                     y accesorios que se adapten a tu estilo personal,
-                    necesidades y ocasiones específicas.
+                    necesidades y ocasiones específicas. Identificaremos
+                    elementos que te gusten y aquellas áreas donde podrías estar
+                    interesado en realizar cambios. Proporcionaré consejos sobre
+                    cómo combinar prendas y accesorios de manera creativa para
+                    crear looks frescos y atractivos donde se adapten a tus
+                    necesidades prácticas, reflejen tu personalidad y estilo de
+                    vida, permitiendote sentirte seguro y cómodo en todas las
+                    ocasiones.
                   </p>
                   <a href={`/contact`} className="button-1">
                     Contáctame<span></span>
@@ -135,16 +141,12 @@ export const MyServices = () => {
               </div>
             </div>
             <div
-              className="col-md-12 mb-90 animate-box"
+              className="col-md-12 mt-90 animate-box"
               data-animate-effect="fadeInUp"
             >
               <div className="item right">
                 <figure>
-                  <img
-                    src={Image7}
-                    alt=""
-                    className="img-fluid"
-                  />
+                  <img src={Image7} alt="" className="img-fluid" />
                 </figure>
                 <div className="caption padding-left">
                   <div className="title">Ocasiones Especiales</div>
@@ -153,7 +155,13 @@ export const MyServices = () => {
                     oportunidad para hacer el mejor recuerdo posible, por eso
                     para una ocasión especial implica un enfoque más específico
                     y detallado para garantizar que tu luzcas de la mejor manera
-                    posible en un evento particular.
+                    posible en un evento particular. Comprenderemos la ocasión
+                    especial, el código de vestimenta, tus gustos, preferencias
+                    y cualquier otra consideración relevante. Para que te
+                    sientas segura de llevar el look que más te favorezca,
+                    prestando especial atención a la selección de accesorios,
+                    como joyería, relojes y bolsos, para garantizar que
+                    complementen y realcen tu atuendo.
                   </p>
                   <a href={`/contact`} className="button-1">
                     Contáctame<span></span>
